92e672ae413c3aad58bed21ea283384eab755f30
Loic Duros <loic.duros@gmail.com>
2018-11-12 17:38:00 -0500

    Generate and output SBDH based on inbound destination/source sgln

--- quartet_tracelink/steps.py
+++ quartet_tracelink/steps.py
@@ -16,6 +16,13 @@ from quartet_output import steps
 from quartet_tracelink.parsing.epcpyyes import get_default_environment
 from quartet_tracelink.parsing.parser import TraceLinkEPCISParser
 from gs123.conversion import URNConverter
+from quartet_output.steps import EPCPyYesOutputStep, ContextKeys
+from quartet_capture.rules import RuleContext
+from EPCPyYes.core.v1_2 import template_events
+from EPCPyYes.core.SBDH import sbdh
+import uuid
+import datetime
+
 
 class AddCommissioningDataStep(steps.AddCommissioningDataStep):
     def process_events(self, events: list):
@@ -43,3 +50,93 @@ class OutputParsingStep(steps.OutputParsingStep):
         Returns the parser that uses the tracelink template EPCPyYes objects.
         """
         return TraceLinkEPCISParser
+
+
+
+class TracelinkOutputStep(EPCPyYesOutputStep):
+    """
+    Will look for any EPCPyYes events in the context and render them to
+    XML or JSON depending on the step parameter configuration.
+    """
+
+    def generate_sbdh(self, header_version = '1.0', sender_sgln = None,
+                      receiver_sgln = None, doc_id_standard='EPCGlobal', doc_id_type_version='1.0',
+                      doc_id_instance_identifier=None, doc_id_type='Events', creation_date_and_time=None):
+        '''
+        Slap in an SBDH.
+        '''
+        if sender_sgln and receiver_sgln:
+            sender = sbdh.Partner(sbdh.PartnerType.SENDER, sbdh.PartnerIdentification('SGLN', sender_sgln))
+            receiver = sbdh.Partner(sbdh.PartnerType.RECEIVER, sbdh.PartnerIdentification('SGLN', receiver_sgln))
+            partner_list = [sender, receiver]
+            return sbdh.StandardBusinessDocumentHeader(partners=partner_list)
+        return None
+        
+    def execute(self, data, rule_context: RuleContext):
+        """
+        Pulls the object, agg, transaction and other events out of the context
+        for processing.  See the step parameters
+        :param data: The original message (not used by this step).
+        :param rule_context: The RuleContext containing any filtered events
+        and also any EPCPyYes events that were created by prior steps.
+        """
+        env = get_default_environment()        
+        self.rule_context = rule_context
+        append_filtered_events = self.get_boolean_parameter(
+            'Append Filtered Events', True)
+        prepend_filtered_events = self.get_boolean_parameter(
+            'Prepend Filtered Events', False
+        )
+        oevents = rule_context.context.get(ContextKeys.OBJECT_EVENTS_KEY.value,
+                                           [])
+        aggevents = rule_context.context.get(
+            ContextKeys.AGGREGATION_EVENTS_KEY.value, [])
+        if append_filtered_events:
+            if prepend_filtered_events:
+                all_events = self.get_filtered_events() + oevents + aggevents
+            else:
+                all_events = oevents + aggevents + self.get_filtered_events()
+        else:
+            all_events = oevents + aggevents
+        sbdh_out = None
+        if len(all_events) > 0:
+            for event in all_events:
+                if event.source_list and event.destination_list:
+                    source_sgln = None
+                    destionation_sgln = None
+                    for source in event.source_list:
+                        if 'owning_party' in source.type:
+                            source_sgln = source.source
+                    for destination in event.destination_list:
+                        if 'owning_party' in destination.type:
+                            destination_sgln = destination.destination
+                    sbdh_out = self.generate_sbdh(sender_sgln=source_sgln, receiver_sgln=destination_sgln)
+                    break
+            epcis_document = template_events.EPCISEventListDocument(all_events, sbdh_out, template=env.get_template('quartet_tracelink/tracelink_epcis_events_document.xml'))
+            if self.get_boolean_parameter('JSON', False):
+                data = epcis_document.render_json()
+            else:
+                data = epcis_document.render()
+            rule_context.context[
+                ContextKeys.OUTBOUND_EPCIS_MESSAGE_KEY.value
+            ] = data
+
+    def declared_parameters(self):
+        return {
+            "Append Filtered Events": _('Whether or not to append any events '
+                                        'found in the rule context under the '
+                                        'FILTERED_EVENTS_KEY to the new '
+                                        'message.'),
+            "Prepend Filtered Events": _('Whether or not to add the filtered '
+                                         'events to the beginning of the '
+                                         'message or the end. If this is True,'
+                                         ' then all filtered events will be'
+                                         'added to the beginning of the EPCIS '
+                                         'EventList- otherwise they will be '
+                                         'added to the end.'),
+            "JSON": _('If set to True then the output message for the EPCPyYEs'
+                      'events will be JSON.'),
+        }
+
+    def on_failure(self):
+        super().on_failure()
