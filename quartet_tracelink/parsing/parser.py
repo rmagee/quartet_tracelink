1943cab765b5b3a57e505cab0d068d0422fee9d7
Serial Lab <slab@serial-lab.com>
2020-03-11 17:56:44 -0600

    parser now instance attribute

--- quartet_tracelink/parsing/parser.py
+++ quartet_tracelink/parsing/parser.py
@@ -14,9 +14,11 @@
 # Copyright 2018 SerialLab Corp.  All rights reserved.
 import logging
 
-from EPCPyYes.core.v1_2 import template_events
+from EPCPyYes.core.v1_2 import template_events, events as yes_events
+from gs123.conversion import URNConverter
 from quartet_output.models import EPCISOutputCriteria
 from quartet_output.parsing import BusinessOutputParser
+from quartet_masterdata.models import Company
 
 from quartet_tracelink.parsing.epcpyyes import get_default_environment
 from lxml import etree
@@ -32,6 +34,7 @@ class TraceLinkEPCISParser(BusinessOutputParser):
         super().__init__(stream, epcis_output_criteria, event_cache_size,
                          recursive_decommission, skip_parsing)
         self.receiver_gln = None
+        self.have_checked_company = False
 
     def get_epcpyyes_object_event(self):
         return template_events.ObjectEvent(
@@ -40,7 +43,20 @@ class TraceLinkEPCISParser(BusinessOutputParser):
             template='quartet_tracelink/disposition_assigned.xml'
         )
 
-    def parse_unexpected_obj_element(self, oevent, child: etree.Element):
-        if "transferredToId" in child.tag:
-            logger.debug('Found a sender GLN')
-            self.receiver_gln = child.text.strip()
+    def handle_object_event(self, epcis_event: yes_events.ObjectEvent):
+        super().handle_object_event(epcis_event)
+        if not self.have_checked_company:
+            self.have_checked_company = True
+            epc = epcis_event.epc_list[0]
+            company_prefix = URNConverter(epc).company_prefix
+            try:
+                company = Company.objects.get(gs1_company_prefix=company_prefix)
+                self.receiver_gln = company.GLN13
+            except Company.DoesNotExist:
+                logger.debug('Could not find the company for company prefix '
+                             '%s ', company_prefix)
+
+    # def parse_unexpected_obj_element(self, oevent, child: etree.Element):
+    #     if "transferredToId" in child.tag:
+    #         logger.debug('Found a sender GLN')
+    #         self.receiver_gln = child.text.strip()
