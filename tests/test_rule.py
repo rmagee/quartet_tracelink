ca841d03957a7700a465f7b5b6ad989f0dfc477d
Serial Lab <slab@serial-lab.com>
2020-06-06 11:01:50 -0600

    updated assertions

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -622,7 +622,7 @@ class TestRules(TransactionTestCase):
                 run_immediately=True
             )
         self.assertEqual(
-            Company.objects.all().count(), 40
+            Company.objects.all().count(), 43
         )
         self.assertEqual(
             Location.objects.all().count(), 42
