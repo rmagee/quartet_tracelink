3357cd026c265a6f578bf409cb419737d39fa7db
Serial Lab <slab@serial-lab.com>
2020-06-06 11:07:35 -0600

    Bump version: 1.0.25 → 1.0.26

--- quartet_tracelink/__init__.py
+++ quartet_tracelink/__init__.py
@@ -14,7 +14,7 @@
 # Copyright 2018 SerialLab Corp.  All rights reserved.
 import os
 
-__version__ = '1.0.25'
+__version__ = '1.0.26'
 
 TEMPLATES_PATH = os.path.join(os.path.dirname(__file__),
                               'templates/quartet_tracelink')
